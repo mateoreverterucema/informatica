b29c77a2fadb89fe8d134eb07534889db70b6cff
mateoreverterucema <mreverter24@ucema.edu.ar>
2022-09-20 12:02:56 -0300

    test

---
+++ Desktop/UCEMA/2° Año/2do Semestre/Programación Web/PracticaJS/fundamentosjs/src/App.js
@@ -0,0 +1,24 @@
+
+
+function App() {
+
+const nombre = "Mateo"
+
+const apellido = "Reverter"
+
+let edad = 19
+
+edad = 20
+
+console.log(nombre, apellido, edad)
+
+console.log("Hola", nombre)
+
+  return (
+    <>
+      <h1>const - let - var</h1>
+    </>
+  );
+}
+
+export default App;
